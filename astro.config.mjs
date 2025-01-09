96de5a60d29957084e592309750823536509317b
mwatermelon <m.watermelon20@gmail.com>
2025-01-09 10:12:54 -0600

    ...build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site:'https://m-watermelon.github.io/',
-    base:'Market-Site',
+    base:'Market-Site/',
 });
